bbf48509542a1b2582cd4728611eea48771e68b6
Jorge Ramirez <Jorge.Ramirez@D2L.com>
2021-05-17 11:50:33 -0400

    Create localize mixin

--- src/components/pages/cepr-user-selection-page.js
+++ src/components/pages/cepr-user-selection-page.js
@@ -1,15 +1,9 @@
 import '../text/cepr-page-title';
 
 import { html, LitElement } from 'lit-element/lit-element.js';
-import { LocalizeDynamicMixin } from '@brightspace-ui/core/mixins/localize-dynamic-mixin.js';
+import { LocalizeMixin } from '../../mixins/localize-mixin';
 
-class CeprUserSelectionPage extends LocalizeDynamicMixin(LitElement) {
-	static get localizeConfig() {
-		return {
-			importFunc: async lang => (await import(`../../../lang/${lang}.js`)).default
-		};
-	}
-
+class CeprUserSelectionPage extends LocalizeMixin(LitElement) {
 	render() {
 		return html`
 			<cepr-page-title>${this.localize('toolTitle')}</cepr-page-title>
