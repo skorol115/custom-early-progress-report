4dd5ce43a952043bb074c3461457f95ad3211bf1
Jorge Ramirez <Jorge.Ramirez@D2L.com>
2021-05-17 13:03:39 -0400

    Use brightspace styles for page header

--- src/components/pages/cepr-user-selection-page.js
+++ src/components/pages/cepr-user-selection-page.js
@@ -1,12 +1,15 @@
-import '../text/cepr-page-title';
-
 import { html, LitElement } from 'lit-element/lit-element.js';
+import { heading1Styles  } from '@brightspace-ui/core/components/typography/styles.js';
 import { LocalizeMixin } from '../../mixins/localize-mixin';
 
 class CeprUserSelectionPage extends LocalizeMixin(LitElement) {
+	static get styles() {
+		return [ heading1Styles ];
+	}
+
 	render() {
 		return html`
-			<cepr-page-title>${this.localize('toolTitle')}</cepr-page-title>
+			<h1 class="d2l-heading-1">${this.localize('toolTitle')}</h1>
 			${this.localize('toolDescription')}
 		`;
 	}
