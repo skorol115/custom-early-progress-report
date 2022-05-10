cacefd616777130f304309fc7804c9010654cad3
Andrii Slobodianyk <59312361+Bumpie7@users.noreply.github.com>
2022-05-10 13:05:05 +0300

    Review

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,9 +1,9 @@
 const API_VERSION = '1.0';
 export class Routes {
-	static AllUsers(orgUnitId, isSearchAllCriteria) { return `/d2l/api/customization/${API_VERSION}/${orgUnitId}/epr/users?searchOption=${isSearchAllCriteria}`; }
+	static AllUsers(orgUnitId, searchOption) { return `/d2l/api/customization/${API_VERSION}/${orgUnitId}/epr/users?searchOption=${searchOption}`; }
 	static GradeItems(orgUnitId) { return `/d2l/api/customization/${API_VERSION}/${orgUnitId}/epr/gradeitems`; }
-	static NumUsers(orgUnitId, searchTerm, isSearchAllCriteria) { return `/d2l/api/customization/${API_VERSION}/${orgUnitId}/epr/users/count?${searchTerm ? `searchTerm=${searchTerm}` : ''}&searchOption=${isSearchAllCriteria}`; }
+	static NumUsers(orgUnitId, searchTerm, searchOption) { return `/d2l/api/customization/${API_VERSION}/${orgUnitId}/epr/users/count?${searchTerm ? `searchTerm=${searchTerm}` : ''}&searchOption=${searchOption}`; }
 	static Preferences() { return `/d2l/api/customization/${API_VERSION}/epr/userpreferences`; }
 	static Records(orgUnitId) { return `/d2l/api/customization/${API_VERSION}/${orgUnitId}/epr/records`; }
-	static Users(orgUnitId, pageNumber, pageSize, sortField, sortDesc, searchTerm, isSearchAllCriteria) { return `/d2l/api/customization/${API_VERSION}/${orgUnitId}/epr/users?sort=${sortField}&desc=${sortDesc}&pageNumber=${pageNumber}&pageSize=${pageSize}${searchTerm ? `&searchTerm=${searchTerm}` : ''}&searchOption=${isSearchAllCriteria}`; }
+	static Users(orgUnitId, pageNumber, pageSize, sortField, sortDesc, searchTerm, searchOption) { return `/d2l/api/customization/${API_VERSION}/${orgUnitId}/epr/users?sort=${sortField}&desc=${sortDesc}&pageNumber=${pageNumber}&pageSize=${pageSize}${searchTerm ? `&searchTerm=${searchTerm}` : ''}&searchOption=${searchOption}`; }
 }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -2,13 +2,13 @@ import { EprRequests } from '../api/epr-requests';
 
 export class UserService {
 
-	static async getAllUsers(orgUnitId, gradeItemQueries = [], isSearchAllCriteria = false) {
-		const allUsers = await EprRequests.getAllUsers(orgUnitId, gradeItemQueries, +isSearchAllCriteria);
+	static async getAllUsers(orgUnitId, gradeItemQueries = [], searchOption = false) {
+		const allUsers = await EprRequests.getAllUsers(orgUnitId, gradeItemQueries, +searchOption);
 		return allUsers;
 	}
 
-	static async getNumUsers(orgUnitId, gradeItemQueries = [], searchTerm = '', isSearchAllCriteria = false) {
-		const numUsers = await EprRequests.getNumUsers(orgUnitId, gradeItemQueries, searchTerm, +isSearchAllCriteria);
+	static async getNumUsers(orgUnitId, gradeItemQueries = [], searchTerm = '', searchOption = false) {
+		const numUsers = await EprRequests.getNumUsers(orgUnitId, gradeItemQueries, searchTerm, +searchOption);
 		return numUsers;
 	}
 
@@ -17,13 +17,13 @@ export class UserService {
 		return preferences;
 	}
 
-	static async getUsers(orgUnitId, pageNumber, pageSize, sortField = 0, sortDesc = false, gradeItemQueries = [], searchTerm = '', isSearchAllCriteria = false) {
-		const users = await EprRequests.getUsers(orgUnitId, pageNumber, pageSize, sortField, sortDesc, gradeItemQueries, searchTerm, +isSearchAllCriteria);
+	static async getUsers(orgUnitId, pageNumber, pageSize, sortField = 0, sortDesc = false, gradeItemQueries = [], searchTerm = '', searchOption = false) {
+		const users = await EprRequests.getUsers(orgUnitId, pageNumber, pageSize, sortField, sortDesc, gradeItemQueries, searchTerm, +searchOption);
 		return users;
 	}
 
-	static async setUserPreferences(isSearchAllCriteria) {
-		const response = await EprRequests.setPreferences(+isSearchAllCriteria);
+	static async setUserPreferences(searchOption) {
+		const response = await EprRequests.setPreferences(+searchOption);
 		return response;
 	}
 }
